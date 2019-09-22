994dbb95eed75f94603a111b8d2113aa1c2091ec
hai-qing <55316054+hai-qing@users.noreply.github.com>
2019-09-22 14:57:52 +0800

    完善代码
    完善代码，实现字符串逆序输出

--- string.c
+++ string.c
@@ -2,9 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+void  Inverse (char str[],char ptr[]);
+
 int main()
 {
-  
+  char str[100];
+  char ptr[100];
+  printf("Input a string:");
+  gets(str);
+  Inverse(str,ptr);
+  printf("Inversed results :%s\n",ptr);
+}
 
+void Inverse (char str[],char ptr[])
+{
+  int n = strlen (str);
+  int i;
+  for (i=0;i<=n;i++)
+  {
+   ptr[i]=str[n-i-1];
+  }
+ ptr[i-1]='\0';
 
 }
